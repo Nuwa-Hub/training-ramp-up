df30e0f3579a30736b8214d9c28a94ba8b2e3554
Nuwa-Hub <you@example.com>
2023-01-05 09:01:35 +0530

    create add and edit data

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from '@progress/kendo-react-buttons';
+
+import './App.css';
+import { Table } from './pages/Table';
+
+function App() {
+ 
+  return (
+    <div className="App">
+     <Table/>
+    </div>
+  );
+}
+
+export default App;
